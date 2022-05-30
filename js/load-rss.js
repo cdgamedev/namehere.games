10427a8126ff5dddf8d7331ac370574809301741
Hayley Davies <cd230099@falmouth.ac.uk>
2022-05-30 02:06:54 +0100

    Edited how data is parsed for rss

--- js/load-rss.js
+++ js/load-rss.js
@@ -2,30 +2,22 @@ element = document.getElementById("devlogs");
 element.innerHTML = "Either the RSS URL is broken, or there are no published devlogs.";
 
 const RSS_URL = `./devlog.rss`;
+const re = new RegExp(`https:\/\/img.itch.zone\/(.*)" `);
 
 fetch(`./devlog.rss`)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then(data => console.log(data))
+    .then(data => parseRSS(data))
 
-function parse_rss(data) {
+function parseRSS(data) {
     const items = data.querySelectorAll("item");
     let html = ``;
-
     items.forEach(el => {
+        let description = re.exec(el.querySelector("description").innerHTML);
+        let imageURL = description[0].replace(`" `, "");
         html += `
         <article>
-          <img src="${el.querySelector("description").innerHTML}" alt="">
-          <h2>
-            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
-              ${el.querySelector("title").innerHTML}
-            </a>
-          </h2>
-        </article>
-        `;
-        html += `
-        <article>
-          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+          <img src="${imageURL}" alt="">
           <h2>
             <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
               ${el.querySelector("title").innerHTML}
