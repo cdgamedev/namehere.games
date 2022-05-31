d4fa79b78de6347cc71df076ee854fd25f1b1530
Hayley Davies <cd230099@falmouth.ac.uk>
2022-05-31 13:31:04 +0100

    Minor changes to page
    - Updated styling of the team's profile images
    - Updated the load-rss.js to have some null checks etc

--- js/load-rss.js
+++ js/load-rss.js
@@ -1,16 +1,20 @@
 element = document.getElementById("devlogs");
-element.innerHTML = "Either the RSS URL is broken, or there are no published devlogs.";
+element.innerHTML = "Hm, it seems the RSS URL hasn't been found.";
 
 const RSS_URL = `./devlog.rss`;
 const re = new RegExp(`https:\/\/img.itch.zone\/(.*)" `);
 const MAX_DEVLOGS_TO_SHOW = 10;
 
-fetch(`./devlog.rss`)
+fetch(RSS_URL)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => parseRSS(data))
 
 function parseRSS(data) {
+    if (data.querySelectorAll("rss").length == 0) {
+        return;
+    }
+
     const items = data.querySelectorAll("item");
     let html = ``;
     let devlogsToShow = items.length;
@@ -18,10 +22,21 @@ function parseRSS(data) {
         devlogsToShow = MAX_DEVLOGS_TO_SHOW;
     }
 
+    if (devlogsToShow == 0) {
+        element = document.getElementById("devlogs");
+        element.innerHTML = "There are no devlogs to display.";
+        return;
+    }
+
     for(let i = 0; i < devlogsToShow; i++) {
         let el = items[i];
         let description = re.exec(el.querySelector("description").innerHTML);
-        let imageURL = description[0].replace(`" `, "");
+        let imageURL = "";
+        if (description == null) {
+            imageURL = "./img/game/game-logo.png";
+        } else {
+            imageURL = description[0].replace(`" `, "");
+        }
 
         html += `
         <article>
