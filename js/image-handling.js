b63af5803584be120aba6db8adbf6e740cbff804
Hayley Davies <cd230099@falmouth.ac.uk>
2022-06-06 06:08:02 +0100

    Minor rework of image-handling.js
    - Remove need for offset (just rename the file to start at 0).
    - Removed video embed from page directly - this is generated on load now.
    - Added video focusing to image-handling.js.
    - Created new function - createHighlightStrip - to image-handling.js which calls embedYouTube() and createImages() directly as they use the same parentId.
    - Renamed screenshot-5.png to screenshot-0.png.

--- js/image-handling.js
+++ js/image-handling.js
@@ -1,42 +1,52 @@
-const youtubeVideos = [
+const developmentVideos = [
     "BES0WkYXtyE"
 ]
 
+var highlightedImageId = "";
+
 window.onload = function() {
-    embedYouTube("current-development", youtubeVideos)
-    createImages("current-development", './img/game/screenshot-', 5, 1);
+    createHightlightStrip("current-development", developmentVideos, './img/game/screenshot-', 5)
+}
+
+function createHightlightStrip(parentId, youtubeVideos, imageBaseURL, numberOfImages) {
+    if (youtubeVideos.length > 0) {
+        embedYouTube(parentId, youtubeVideos)
+    }
+    if (numberOfImages > 0) {
+        createImages(parentId, imageBaseURL, numberOfImages);
+    }
 }
 
 function focusImage(parentId, imageIndex) {
-    let largeElement = document.getElementById(`${parentId}-large`);
     let elementId = `${parentId}-${imageIndex}`;
+
+    if (highlightedImageId == elementId) {
+        return;
+    }
+
+    let largeElement = document.getElementById(`${parentId}-large`);
     let imageElement = document.getElementById(elementId);
+    
+    highlightedImageId = elementId;
 
     largeElement.innerHTML = '';
     largeElement.appendChild(imageElement.cloneNode(true));
 
-    let parentElement = document.getElementById(`${parentId}-showcase`);
-    parentElement.childNodes.forEach(child => {
-        if (child.tagName == "IMG") {
-            child.className = "";
-        }
-    });
-
-    imageElement.className = "selected";
+    updateBorder(parentId, imageElement);
 }
 
-function createImages(parentId, baseURL, numberOfImages, offset) {
+function createImages(parentId, baseURL, numberOfImages) {
     let parentElement = document.getElementById(`${parentId}-showcase`);
     for (let i = 0; i < numberOfImages; i++) {
         let span = document.createElement("span");
-        let elementId = `${parentId}-${i + offset}`;
+        let elementId = `${parentId}-${i}`;
         let element = document.createElement("img");
         
         span.className = "video-thumbnail";
 
         element.id = elementId;
-        element.src = `${baseURL}${i + offset}.png`;
-        element.onclick = function() { focusImage(parentId, i + offset) };
+        element.src = `${baseURL}${i}.png`;
+        element.onclick = function() { focusImage(parentId, i) };
         span.appendChild(element);
 
         parentElement.appendChild(span);
@@ -54,12 +64,57 @@ function embedYouTube(parentId, videoIdList) {
         span.innerHTML = `
             <i class="fa-solid fa-circle fa-stack-lg stack-bottom"></i>
             <i class="fa-solid fa-circle-play fa-stack-lg stack-top"></i>`;
+        
+        img.onclick = function() { focusVideo(parentId, video) };
+        img.id = elementId;
 
         span.className = "video-thumbnail";
-        span.id = elementId;
         span.appendChild(img);
 
         img.src = `https://img.youtube.com/vi/${video}/0.jpg`
         parentElement.appendChild(span);
+
+        if (videoIdList[0] == video) {
+            img.onclick.apply();
+        }
     });
+}
+
+function focusVideo(parentId, youtubeVideoId) {
+    let elementId = `${parentId}-${youtubeVideoId}`;
+
+    if (highlightedImageId == elementId) {
+        return;
+    }
+
+    let largeElement = document.getElementById(`${parentId}-large`);
+    let imageElement = document.getElementById(elementId);
+    let iframe = document.createElement("iframe");
+
+    highlightedImageId = elementId;
+
+    iframe.src = `https://www.youtube.com/embed/${youtubeVideoId}`;
+    iframe.title = "YouTube video player";
+    iframe.frameBorder = "0";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+
+    largeElement.innerHTML = '';
+    largeElement.appendChild(iframe);
+
+    updateBorder(parentId, imageElement);
+}
+
+function updateBorder(parentId, imageElement) {
+    let parentElement = document.getElementById(`${parentId}-showcase`);
+    parentElement.childNodes.forEach(child => {
+        if (child.tagName == "SPAN") {
+            let img = child.lastChild;
+            if (img.id != imageElement.id) {
+                img.classList.remove("selected");
+            }
+        }
+    });
+
+    imageElement.classList.add("selected");
 }
