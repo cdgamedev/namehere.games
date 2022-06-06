6c971348870966867b2021fa014201af737a3156
Hayley Davies <cd230099@falmouth.ac.uk>
2022-06-06 05:06:52 +0100

    Video thumbnail now gets show
    - Thumbnail has a play button above it which is centered to the image
    - Changed how images are displayed, they now are all under their own span

--- js/image-handling.js
+++ js/image-handling.js
@@ -1,4 +1,9 @@
+const youtubeVideos = [
+    "BES0WkYXtyE"
+]
+
 window.onload = function() {
+    embedYouTube("current-development", youtubeVideos)
     createImages("current-development", './img/game/screenshot-', 5, 1);
 }
 
@@ -12,7 +17,9 @@ function focusImage(parentId, imageIndex) {
 
     let parentElement = document.getElementById(`${parentId}-showcase`);
     parentElement.childNodes.forEach(child => {
-        child.className = "";
+        if (child.tagName == "IMG") {
+            child.className = "";
+        }
     });
 
     imageElement.className = "selected";
@@ -21,11 +28,38 @@ function focusImage(parentId, imageIndex) {
 function createImages(parentId, baseURL, numberOfImages, offset) {
     let parentElement = document.getElementById(`${parentId}-showcase`);
     for (let i = 0; i < numberOfImages; i++) {
+        let span = document.createElement("span");
         let elementId = `${parentId}-${i + offset}`;
         let element = document.createElement("img");
+        
+        span.className = "video-thumbnail";
+
         element.id = elementId;
         element.src = `${baseURL}${i + offset}.png`;
         element.onclick = function() { focusImage(parentId, i + offset) };
-        parentElement.appendChild(element);
+        span.appendChild(element);
+
+        parentElement.appendChild(span);
     }
+}
+
+function embedYouTube(parentId, videoIdList) {
+    let parentElement = document.getElementById(`${parentId}-showcase`);
+
+    videoIdList.forEach(video => {
+        let elementId = `${parentId}-${video}`;
+        let span = document.createElement("span");
+        let img = document.createElement("img");
+
+        span.innerHTML = `
+            <i class="fa-solid fa-circle fa-stack-lg stack-bottom"></i>
+            <i class="fa-solid fa-circle-play fa-stack-lg stack-top"></i>`;
+
+        span.className = "video-thumbnail";
+        span.id = elementId;
+        span.appendChild(img);
+
+        img.src = `https://img.youtube.com/vi/${video}/0.jpg`
+        parentElement.appendChild(span);
+    });
 }
