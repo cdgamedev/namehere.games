58a53a7de81865fa55ff3505951c2bf5dd35a718
Hayley Davies <cd230099@falmouth.ac.uk>
2023-01-28 19:32:31 +0000

    feat(showcases): youtube video embeds now work

--- js/showcase-handler.js
+++ js/showcase-handler.js
@@ -42,7 +42,7 @@ function handleMedia(media, elementId, showcaseId, largeImage) {
     if (media["type"] == "img") {
         mediaElement = handleImage(media, elementId, showcaseId, largeImage);
     } else if (media["type"] == "video") {
-        mediaElement = handleVideo(media, elementId, largeImage);
+        mediaElement = handleVideo(media, elementId, showcaseId, largeImage);
     }
 
     return mediaElement;
@@ -52,6 +52,7 @@ function handleImage(image, elementId, showcaseId, largeImage) {
     let container = createContainerElement();
     let mediaElement = createMediaElement(elementId);
     let description = image["description"];
+
     mediaElement.src = image["url"];
     mediaElement.onclick = function() { focusImage(elementId, showcaseId, largeImage, description) };
 
@@ -59,12 +60,20 @@ function handleImage(image, elementId, showcaseId, largeImage) {
     return container;
 }
 
-function handleVideo(video) {
+function handleVideo(video, elementId, showcaseId, largeImage) {
     let container = createContainerElement();
-    let mediaElement = createMediaElement();
-    let videoId = video["url"];
+    let mediaElement = createMediaElement(elementId);
+    let description = video["description"];
 
-    mediaElement.src = `https://img.youtube.com/vi/${videoId}/0.jpg`;
+    elementId = video["url"];
+    mediaElement.src = `https://img.youtube.com/vi/${elementId}/0.jpg`;
+    mediaElement.onclick = function() { focusVideo(elementId, showcaseId, largeImage, description) };
+
+    container.innerHTML = `
+            <i class="fa-solid fa-circle fa-stack-lg stack-bottom"></i>
+            <i class="fa-solid fa-circle-play fa-stack-lg stack-top"></i>`;
+    container.appendChild(mediaElement);
+
     return container;
 }
 
@@ -82,6 +91,20 @@ function createMediaElement(elementId) {
     return element;
 }
 
+function updateBorders(showcaseId, imageElement) {
+    let parentElement = document.getElementById(`${showcaseId}-showcase`);
+    parentElement.childNodes.forEach(child => {
+        if (child.tagName == "SPAN") {
+            let item = child.lastChild;
+            if (item.id != imageElement.id) {
+                item.classList.remove("selected");
+            }
+        }
+    });
+
+    imageElement.classList.add("selected");
+}
+
 function focusImage(elementId, showcaseId, largeImage, description) {
     let highlightedImageId = largeImage.childNodes[0].id;
 
@@ -94,26 +117,43 @@ function focusImage(elementId, showcaseId, largeImage, description) {
     largeImage.innerHTML = "";
     
     highlightedImageId = elementId;
-    
-    let textElement = document.createElement("p");
-    textElement.innerHTML = description;
 
     largeImage.appendChild(imageElement.cloneNode(true));
+    
+    let textElement = addDescription(description);
     largeImage.appendChild(textElement);
 
     updateBorders(showcaseId, imageElement);
 }
 
-function updateBorders(showcaseId, imageElement) {
-    let parentElement = document.getElementById(`${showcaseId}-showcase`);
-    parentElement.childNodes.forEach(child => {
-        if (child.tagName == "SPAN") {
-            let img = child.lastChild;
-            if (img.id != imageElement.id) {
-                img.classList.remove("selected");
-            }
-        }
-    });
+function focusVideo(elementId, showcaseId, largeImage, description) {
+    let highlightedImageId = largeImage.childNodes[0].id;
 
-    imageElement.classList.add("selected");
+    if (highlightedImageId == elementId) {
+        return;
+    }
+    
+    let imageElement = document.getElementById(elementId);
+
+    highlightedImageId = elementId;
+
+    let iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${elementId}`;
+    iframe.title = "YouTube video player";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+
+    largeImage.innerHTML = '';
+    largeImage.appendChild(iframe);
+    
+    let textElement = addDescription(description);
+    largeImage.appendChild(textElement);
+
+    updateBorders(showcaseId, imageElement);
+}
+
+function addDescription(description) {
+    let descriptionElement = document.createElement("p");
+    descriptionElement.innerHTML = description;
+    return descriptionElement;
 }
